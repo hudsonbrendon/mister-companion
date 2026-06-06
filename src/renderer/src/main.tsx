76fa0fa07302433c6ba688c2bf582c1c7672c231
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2026-06-05 21:00:20 -0300

    feat(theme): apply the stored theme on app boot

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,5 +2,8 @@ import './index.css'
 import './i18n'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { initTheme } from './theme'
+
+initTheme()
 
 createRoot(document.getElementById('root')!).render(<App />)
